42b4727869471c5721b22d85f5c53f24f90f98ab
“Chandan <im.chandan045@gmail.com>
2026-01-25 16:45:47 +0530

    modified vercel.json for deployment

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	// baseURL: "http://localhost:8888",
-	baseURL:"https://foodfly-backend-1.onrender.com/",
+	baseURL:"https://foodfly-backend-1.onrender.com",
 	withCredentials: true,
 });
 
